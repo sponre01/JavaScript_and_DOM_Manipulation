01d2eb14dac63de7e02997eddc49f829c1109cf3
sponre01 <sponre01@gmail.com>
2019-03-03 12:57:19 -0600

    filter table done, not updating yet

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,33 @@
+
+// Level 1: Automatic Table and Date Search
+
+//Fill in the table with unfiltered data
+var tableData = data;
+
+var tbody = d3.select("tbody");
+tableData.forEach(function(UFOreport) {
+  //console.log(UFOreport);
+  var row = tbody.append("tr");
+  Object.entries(UFOreport).forEach(function([key, value]) {
+      //console.log(key, value);
+      var cell = tbody.append("td");
+      cell.text(value)
+  })
+})
+
+// Grab reference to the button element
+var button = d3.select("#filter-btn");
+
+// Define button click
+button.on("click", function() {
+
+  d3.event.preventDefault();
+
+  var inputDate = d3.select("#datetime");
+  var inputText = inputDate.property("value")
+  var filteredData = tableData.filter(x => x.datetime === inputText);
+
+  console.log(filteredData);
+})
+
+// Clear the existing table
